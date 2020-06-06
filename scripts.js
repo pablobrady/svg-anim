755a914c868213d262b04da7a3897283a7f30ceb
Paul Brady <pablo.brady@gmail.com>
2020-06-05 17:39:46 -0700

    Trim down / Tidy up

--- scripts.js
+++ scripts.js
@@ -1,22 +1,6 @@
 const displays = document.querySelectorAll('.note-display');
 const transitionDuration = 900;
 
-displays.forEach(display => {
-  let progress = display.querySelector('.circle__progress--fill');
-  let radius = progress.r.baseVal.value; // <svg><circle r="38"... 
-  let circumference = 2 * Math.PI * radius;
-  let note = parseFloat(display.dataset.note);
-  let offset = circumference * (10 - note) / 10;
-
-  progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
-  progress.style.setProperty('--initialStroke', circumference);
-
-  setTimeout(() => progress.style.strokeDashoffset = offset, 100)
-
-  // strokeTransition(() => progress.style.strokeDashoffset = offset, 100);
-  strokeTransition( display, note);
-});
-
 function strokeTransition(display, note) {
   let progress = display.querySelector('.circle__progress--fill');
   let radius = progress.r.baseVal.value;
@@ -42,12 +26,22 @@ function increaseNumber(display, number, className) {
 }
 
 displays.forEach(display => {
+  let progress = display.querySelector('.circle__progress--fill');
+  let radius = progress.r.baseVal.value; // <svg><circle r="38"... 
+  let circumference = 2 * Math.PI * radius;
   let note = parseFloat(display.dataset.note);
   let [int, dec] = display.dataset.note.split('.');
   [int, dec] = [Number(int), Number(dec)];
 
-  strokeTransition(display, note);
+  let offset = circumference * (10 - note) / 10;
 
+  progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
+  progress.style.setProperty('--initialStroke', circumference);
+
+  setTimeout(() => progress.style.strokeDashoffset = offset, 100)
+
+  strokeTransition( display, note);
   increaseNumber(display, int, 'int');
   increaseNumber(display, dec, 'dec');
 });
+
