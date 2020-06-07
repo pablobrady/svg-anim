e3362c2a662916cd6b45dd56a81d91953b80a7e1
Paul Brady <pablo.brady@gmail.com>
2020-06-06 18:10:48 -0700

    WIP

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const displays = document.querySelectorAll('.note-display');
-const transitionDuration = 900;
+const transitionDuration = 6000;
+const totalRange = 100;
 
 function strokeTransition(display, note) {
   let progress = display.querySelector('.circle__progress--fill');
   let radius = progress.r.baseVal.value;
   let circumference = 2 * Math.PI * radius;
-  let offset = circumference * (10 - note) / 10;
+  let offset = circumference * (totalRange - note) / totalRange;
 
   progress.style.setProperty('--initialStroke', circumference);
   progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
@@ -13,14 +14,13 @@ function strokeTransition(display, note) {
 
 function increaseNumber(display, number, className) {
   let element = display.querySelector(`.percent__${className}`),
-    decPoint = className === 'int' ? '.' : '',
     interval = transitionDuration / number,
     counter = 0;
 
   let increaseInterval = setInterval(() => {
     if (counter === number) { window.clearInterval(increaseInterval); }
 
-    element.textContent = counter + decPoint;
+    element.textContent = counter;
     counter++;
   }, interval);
 }
@@ -29,11 +29,13 @@ displays.forEach(display => {
   let progress = display.querySelector('.circle__progress--fill');
   let radius = progress.r.baseVal.value; // <svg><circle r="38"... 
   let circumference = 2 * Math.PI * radius;
+  let isMPH = display.dataset.mph;
+  console.log("isMPH = " + isMPH)
   let note = parseFloat(display.dataset.note);
-  let [int, dec] = display.dataset.note.split('.');
-  [int, dec] = [Number(int), Number(dec)];
+  let int = Number(display.dataset.note);
+  int = isMPH ? int*1.5 : int*25;
 
-  let offset = circumference * (10 - note) / 10;
+  let offset = circumference * (totalRange - note) / totalRange;
 
   progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
   progress.style.setProperty('--initialStroke', circumference);
@@ -42,6 +44,6 @@ displays.forEach(display => {
 
   strokeTransition( display, note);
   increaseNumber(display, int, 'int');
-  increaseNumber(display, dec, 'dec');
+
 });
 
